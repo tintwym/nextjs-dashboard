c38faf8abc931a8363fc9497c922b588cde6c8d1
TintWaiYanMin <tintwaiyanmin.sg@gmail.com>
2026-08-21 18:41:03 +0800

    lesson 14 - adding authentication

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,7 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
+  trustHost: true,
   pages: {
     signIn: '/login',
   },
@@ -10,12 +11,12 @@ export const authConfig = {
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false;
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [],
 } satisfies NextAuthConfig;
